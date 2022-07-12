37a7f525d6ad023315fc927a6d5eee62d31af990
maihuy243 <maihuy243@gmail.com>
2022-07-12 16:20:24 +0700

    a

--- Javascript/Day7/index.js
+++ Javascript/Day7/index.js
@@ -1,37 +1,26 @@
-function loadData(callback) {
-  fetch("http://localhost:3000/users")
-    .then((res) => res.json())
-    .then(callback);
+async function getData() {
+  const response = await fetch("http://localhost:3000/users");
+  return response.json();
 }
 
 const renderData = (users) => {
   const root = document.querySelector("#root");
-  let htmls = ``;
-  users.map(
+  return users.map(
     ({ name, email, avatar, address: { street, suite, city }, phone }) => {
-      return (htmls += `
-    <div class="card p-4 m-2 ">
-      <div
-        class="image d-flex flex-column justify-content-center align-items-center"
-      >
-        <button class="bg-logo">
-          <img class="logo" src="${avatar}" height="100" width="100" />
-        </button>
-        <span class="name mt-3">${name}</span>
-        <div class="phone">${phone}</div>
-        <div
-          class="d-flex flex-column text-center justify-content-center align-items-center gap-2"
-        >
-          <span class="idd1">${email}</span>
-          <span class="adress">Adress: ${street} ${suite} ${city}</span>
+      return (root.innerHTML += `
+    <div class="card d-flex flex-column  justify-content-center align-items-center p-3 m-3 text-center ">
+      <div class="image">
+        <div class="bg-logo">
+          <img class="logo" src="${avatar}" height="150" width="150" />
         </div>
       </div>
+        <div class="name mt-3">${name}</div>
+        <div class="phone">${phone}</div>
+          <div class="idd1">${email}</div>
+          <div class="adress">Adress: ${street} ${suite} ${city}</div>
     </div>
-    
     `);
     }
   );
-  root.innerHTML = htmls;
 };
-
-loadData((data) => renderData(data));
+getData().then((data) => renderData(data));
---
+++ Javascript/Day8/index.js
@@ -0,0 +1,98 @@
+//CRUD
+// 1. Create  : Tạo Data         / POST
+// 2. Read    : Read Data        / GET
+// 3. Updata  : Update Data PUT  / PATCH
+// 4. Delete  : Delete Data      / DELETE
+
+const get = document.querySelector.bind(document);
+const gets = document.querySelectorAll.bind(document);
+const panigation = gets(".item");
+const view = get(".view");
+const prevBtn = get(".prev");
+const nextBtn = get(".next");
+
+//Call API
+const API = "https://jsonplaceholder.typicode.com/users";
+async function getData() {
+  const response = await fetch(API);
+  const listsUser = await response.json();
+  return listsUser;
+}
+
+const renderData = (lists, index = 0) => {
+  const newLists = lists.slice(Number(index), Number(index) + 2);
+  view.innerHTML = "";
+  newLists.map(({ id, username, email, address: { street, city }, phone }) => {
+    return (view.innerHTML += `
+    <tr>
+      <td>${id}</td>
+      <td>${username}</td>
+      <td>${email}</td>
+      <td>${street} ${city}</td>
+     <td>${phone}</td>
+     <td>
+        <button class="btn btn-primary" onclick="handleEdit(${id})">Edit</button>
+       <button class="btn btn-warning onclick="handleDel(${id})">Del</button>
+      </td>
+    </tr>
+    `);
+  });
+};
+
+const renderDataSearch = (lists) => {
+  view.innerHTML = "";
+  lists.map(({ id, username, email, address: { street, city }, phone }) => {
+    return (view.innerHTML += `
+    <tr>
+      <td>${id}</td>
+      <td>${username}</td>
+      <td>${email}</td>
+      <td>${street} ${city}</td>
+     <td>${phone}</td>
+     <td>
+        <button class="btn btn-primary" onclick="handleEdit(${id})">Edit</button>
+       <button class="btn btn-warning onclick="handleDel(${id})">Del</button>
+      </td>
+    </tr>
+    `);
+  });
+};
+
+//panigation
+const handlePagination = (datas) => {
+  renderData(datas, (newIndex = 0));
+  panigation.forEach((e) => {
+    e.onclick = () => {
+      get(".item.active").removeAttribute("disabled");
+      get(".item.active").classList.remove("active");
+      e.classList.add("active");
+      e.setAttribute("disabled", "");
+      let newIndex = e.dataset.item;
+      renderData(datas, newIndex);
+    };
+  });
+};
+
+const handleSearch = () => {
+  if (get(".searchText").value && get(".keys").value) {
+    getData().then((datas) => handleSearchItem(datas));
+  } else getData().then((datas) => handlePagination(datas));
+};
+
+//handle Search
+const handleSearchItem = (data) => {
+  const dataSearch = get(".searchText").value;
+  const keys = get(".keys").value;
+  const newList = [];
+  data.forEach((user) => {
+    user[keys].includes(dataSearch) ? newList.push(user) : user;
+  });
+  console.log(newList);
+  renderDataSearch(newList);
+};
+
+//start
+function start() {
+  getData().then((datas) => handlePagination(datas));
+}
+start();
